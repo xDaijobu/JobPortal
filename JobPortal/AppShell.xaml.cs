25be8ff1136a08ed4811628f63419b116a0fac64
xDaijobu <christwurangian@gmail.com>
2022-03-31 08:52:09 +0700

    initial commit

---
+++ JobPortal/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using JobPortal.ViewModels;
+using JobPortal.Views;
+using Xamarin.Forms;
+
+namespace JobPortal
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ JobPortal/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using JobPortal.ViewModels;
+
+namespace JobPortal.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
